932a5bf266576cf3895589c74b1b1c9e395065f6
Matthew Incardona <matthewincardona@cias-jmssac.main.ad.rit.edu>
2023-02-20 13:42:20 -0500

    Empty gitignore file

---
+++ _site/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+    './*.md',
+    ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)"
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        heading: ['Inter', 'sans-serif']
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
